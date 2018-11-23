90b7db5de05b25f37662e3f4414de2399b7ec4a0
Anders Sveen <anders@f12.no>
2018-11-23 20:23:24 +0100

    Fixed running tests

---
+++ src/test/kotlin/WritingTestDslTest.kt
@@ -0,0 +1,53 @@
+import org.hamcrest.MatcherAssert
+import org.hamcrest.core.IsEqual
+import org.junit.jupiter.api.Test
+import java.time.LocalTime
+
+class WritingTestDslTest {
+
+    @Test
+    fun testThatDeliveryTimesIsForTheNextWholeHour() {
+        testConfig {
+            time = "09:34"
+        } whenAvailabilityInformation {
+            fromAddress = "Keysers Gate 13, 0186 Oslo, Norway"
+            toAddress = "Bjarertveien 17, 0579 Oslo, Norway"
+        } assertValidDeliveryTimes {
+            Pair(LocalTime.of(10, 0), LocalTime.of(13, 0))
+        }
+    }
+
+}
+
+fun testConfig(setupFunction: TestSetup.() -> Unit): TestSetup {
+    return TestSetup().apply(setupFunction)
+}
+
+
+class TestSetup {
+    lateinit var time: String
+
+    infix fun whenAvailabilityInformation(function: TestRequestInfo.() -> Unit): TestRequestInfo {
+        return TestRequestInfo(this).apply(function)
+    }
+
+}
+
+class TestRequestInfo(private val testSetup: TestSetup) {
+    lateinit var fromAddress: String
+    lateinit var toAddress: String
+
+    infix fun assertValidDeliveryTimes(function: () -> Pair<LocalTime, LocalTime>) {
+        MatcherAssert.assertThat(businessFunction(testSetup.time, fromAddress, toAddress), IsEqual(function()))
+    }
+
+}
+
+
+/**
+ * This should really be the production code that you're running
+ */
+fun businessFunction(time: String, fromAddress: String, toAddress: String): Pair<LocalTime, LocalTime> {
+    Clock.fi
+    return LocalTime.of(10, 0) to LocalTime.of(13, 0)
+}
